5f4accbea976e71d8bef4a8c0cdfe31e91882832
Martynas Seskaitis <mseskaitis@gmail.com>
2020-12-21 14:43:45 +0100

    day 21

---
+++ day-21/script1.py
@@ -0,0 +1,54 @@
+import re
+from functools import reduce
+
+
+def extract_line_info(line):
+    i, a = line.split("(contains")
+    ingredients = re.findall("(\\w+)", i)
+    allergens = re.findall("(\\w+)", a)
+    return set(ingredients), set(allergens)
+
+def simplify_list(unstable):
+    stable = dict()
+    alle = set()
+    while unstable:
+        unstable_copy = unstable.copy()
+        for key, value in unstable_copy.items():
+            unstable[key] = value.difference(alle)
+            if len(unstable[key]) == 1:
+                v = unstable.pop(key)
+                stable[key] = v
+                alle = alle.union(v)
+    return stable
+
+def main():
+    with open("input.txt", "r") as f:
+        lines = f.read().splitlines()
+    foods = []
+    for line in lines:
+        food_ingredients, food_allergens = extract_line_info(line)
+        foods.append({
+            "ingredients": food_ingredients,
+            "allergens":   food_allergens
+            })
+    ingredients = reduce(lambda a, b: a.union(b), [x["ingredients"] for x in foods])
+    allergens = reduce(lambda a, b: a.union(b), [x["allergens"] for x in foods])
+    res = {}
+    for a in allergens:
+        ing = set()
+        for food in foods:
+            if a in food["allergens"]:
+                if not ing:
+                    ing = food["ingredients"]
+                else:
+                    ing = ing.intersection(food["ingredients"])
+        res[a] = ing
+    ll = simplify_list(res)
+    allergenic_ing = reduce(lambda a, b: a.union(b), ll.values())
+    count = 0
+    for food in foods:
+        count += len(food["ingredients"].difference(allergenic_ing))
+    print("Result: %s" % count)
+
+if __name__ == "__main__":
+    main()
